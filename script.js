bd11c313fdf1d499bfb6713fcc9d217838521a0c
insxne1337 <romangamers2009@gmail.com>
2024-10-21 21:31:19 +0300

    continue

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// Получаем элементы кнопок и модального окна
 const openModal = document.getElementById("openModal");
 const closeModal = document.getElementById("closeModal");
 const modal = document.getElementById("modal");
 
-// Открытие модального окна при клике на кнопку "Открыть"
 openModal.addEventListener("click", function () {
   modal.classList.add("is-open");
 });
 
-// Закрытие модального окна при клике на кнопку "Закрыть"
 closeModal.addEventListener("click", function () {
   modal.classList.remove("is-open");
 });
 
-// Закрытие модального окна при клике вне его содержимого
 modal.addEventListener("click", function (e) {
   if (e.target === modal) {
     modal.classList.remove("is-open");
